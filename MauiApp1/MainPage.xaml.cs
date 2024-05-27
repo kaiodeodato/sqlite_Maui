9f1dd6e8759ac183fd4ab23d938022c0cb2198de
kaiodeodato <kaiod.oficial@gmail.com>
2024-05-27 15:14:29 +0100

    first commit

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly LocalDBService _dbService;
+        private int _editCustomerId;
+
+        public MainPage(LocalDBService dbService)
+        {
+            InitializeComponent();
+            _dbService = dbService;
+            Task.Run(async () => listView.ItemsSource = await _dbService.GetCustomers());
+        }
+               
+        private async void saveButton_Clicked(object sender, EventArgs e)
+        {
+            if(_editCustomerId == 0)
+            {
+                await _dbService.Create(new Customer
+                {
+                    CustomerName = nameEntryField.Text,
+                    Email = emailEntryField.Text,
+                    Mobile = mobileEntryField.Text,
+                });
+            }
+            else
+            {
+                await _dbService.Update(new Customer
+                {
+                    Id = _editCustomerId,
+                    CustomerName = nameEntryField.Text,
+                    Email = emailEntryField.Text,
+                    Mobile = mobileEntryField.Text,
+                });
+
+                _editCustomerId = 0;
+            }
+
+            nameEntryField.Text = string.Empty;
+            emailEntryField.Text = string.Empty;
+            mobileEntryField.Text = string.Empty;
+
+            listView.ItemsSource = await _dbService.GetCustomers();
+        }
+
+        private async void listView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var customer = (Customer)e.Item;
+            var action = await DisplayActionSheet("Action", "Cancel", null, "Edit", "Delete");
+
+            switch (action)
+            {
+                case "Edit":
+                    _editCustomerId = customer.Id;
+                    nameEntryField.Text = customer.CustomerName;
+                    emailEntryField.Text = customer.Email;
+                    mobileEntryField.Text = customer.Mobile;
+
+                    break;
+                case "Delete":
+
+                    await _dbService.Delete(customer);
+                    listView.ItemsSource = await _dbService.GetCustomers();
+
+                    break;
+            }
+        }
+    }
+
+}
